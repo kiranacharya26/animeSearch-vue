3a5f6031a4b834cd964ecfa6aa59a97c44574246
kiranacharya26 <acharya.kiran26ka@gmail.com>
2022-08-10 13:53:17 +0530

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header>
+      <h1>My Anime</h1>
+      <h3>Search for your favariote anime here</h3>
+      <form class="search-box" @submit.prevent="handleSearch">
+        <input
+          type="search"
+          class="search-field"
+          placeholder="Search for an anime"
+          required
+          v-model="search"
+        />
+      </form>
+    </header>
+    <main>
+      <div class="cards" v-if="animeList.length > 0">
+        <Card v-for="anime in animeList" :key="anime.mal_id" :anime="anime" />
+      </div>
+      <div v-else class="no-results">
+        <h2>No results</h2>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from '@vue/reactivity'
+import Card from './components/Card.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Card },
+  setup() {
+    const search = ref('')
+    const animeList = ref([])
+
+    const handleSearch = async () => {
+      animeList.value = await fetch(
+        `https://api.jikan.moe/v3/search/anime?q=${search.value}`
+      )
+        .then((res) => res.json())
+        .then((data) => data.results)
+      search.value = ''
+    }
+    return {
+      search,
+      animeList,
+      handleSearch,
+    }
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+header {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+input {
+  max-width: 600px;
+  width: 30rem;
+  height: 2rem;
+}
+.no-results {
+  margin-top: 2rem;
+  margin-left: 2rem;
+}
+.cards {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin-top: 3rem;
+  align-items: center;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <a :href="anime.url">
+      <img :src="anime.image_url" alt="Anime poster" />
+      <h3>{{ anime.title }}</h3>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['anime'],
+}
+</script>
+<style lang="scss">
+a {
+  text-decoration: none;
+}
+img {
+  object-fit: cover;
+}
+.card {
+  text-align: center;
+}
+</style>
